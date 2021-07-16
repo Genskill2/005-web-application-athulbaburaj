294eee58a0c0dcc8582c9665531467a9e1f44211
athulbaburaj <athulbaburajp23@gmail.com>
2021-07-16 20:44:58 +0530

    Made relevant changes for sold date to be updaate in db

--- petshop/pets.py
+++ petshop/pets.py
@@ -74,6 +74,9 @@ def edit(pid):
     elif request.method == "POST":
         description = request.form.get('description')
         sold = request.form.get("sold")
+        if sold=="1":
+                sold = datetime.datetime.now().strftime("%Y-%m-%d")
+                cursor.execute(""" update pet set sold = ? where id = ?;""", (sold, pid))
         cursor.execute("""update pet set description=?;""", [description])
         conn.commit()
         # TODO Handle sold
